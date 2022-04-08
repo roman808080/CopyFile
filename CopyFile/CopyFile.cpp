ff83f523554ecdeb0d438c71f8a57391f7b2335d
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-04-08 13:19:10 +0200

    Removed start and end blocks.

--- CopyFile/CopyFile.cpp
+++ CopyFile/CopyFile.cpp
@@ -42,12 +42,8 @@ int main()
 
     const std::string filePath("CopyFile.cpp"); // copying itself
     const size_t blockSize = Constants::Kilobyte;
-    const auto numberOfBlocks = FileUtils::getPossibleBlocksAmount(filePath, blockSize);
-
-    const size_t startBlock = 0;
-    const uintmax_t endBlock = numberOfBlocks;
-
-    auto fileInfo = std::make_unique<FileInfo>(filePath,blockSize, startBlock, endBlock);
+ 
+    auto fileInfo = std::make_unique<FileInfo>(filePath, blockSize);
     auto inputFile = std::make_shared<InputFile>(std::move(fileInfo));
 
     std::shared_ptr<ThreadsafeQueue<std::vector<char>>> queue(std::make_shared<ThreadsafeQueue<std::vector<char>>>());
