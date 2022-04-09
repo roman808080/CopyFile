da98eb04feabe666a7937d905c0fc8c7b57e93f4
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-04-09 19:33:54 +0200

    Adding reading arguments from console.

--- CopyFile/CopyFile.cpp
+++ CopyFile/CopyFile.cpp
@@ -4,11 +4,20 @@
 #include "App.h"
 #include "Constants.h"
 
-int main()
+int main(int argc, char* argv[])
 {
 	try
 	{
-		App app("CopyFile.cpp", "file.txt", Constants::Megabyte);
+		if (argc < 3)
+		{
+			std::cout << "Not enough parameters. Should be: CopyFile <source> <destination>" << std::endl;
+			return 1;
+		}
+
+		const std::string inputFileName = argv[1];
+		const std::string outputFileName = argv[2];
+
+		App app(inputFileName, outputFileName, Constants::Megabyte);
 		app.run();
 	}
 	catch (const std::exception& exc)
