fbb72efb0afba135dad14328f6d32f675a4ae030
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-05-26 20:48:32 +0200

    Fixed an example.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,20 +1,126 @@
-// Server.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <array>
 #include <iostream>
+#include <memory>
+#include <boost/asio.hpp>
 
-int main()
+using boost::asio::awaitable;
+using boost::asio::buffer;
+using boost::asio::co_spawn;
+using boost::asio::detached;
+using boost::asio::ip::tcp;
+using boost::asio::use_awaitable;
+
+struct proxy_state
 {
-    std::cout << "Hello World!\n";
+  proxy_state(tcp::socket client)
+    : client(std::move(client))
+  {
+  }
+
+  tcp::socket client;
+  tcp::socket server{client.get_executor()};
+};
+
+using proxy_state_ptr = std::shared_ptr<proxy_state>;
+
+awaitable<void> client_to_server(proxy_state_ptr state)
+{
+  try
+  {
+    std::array<char, 1024> data;
+
+    for (;;)
+    {
+      auto n = co_await state->client.async_read_some(buffer(data), use_awaitable);
+
+      co_await async_write(state->server, buffer(data, n), use_awaitable);
+    }
+  }
+  catch (const std::exception& e)
+  {
+    state->client.close();
+    state->server.close();
+  }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+awaitable<void> server_to_client(proxy_state_ptr state)
+{
+  try
+  {
+    std::array<char, 1024> data;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (;;)
+    {
+      auto n = co_await state->server.async_read_some(buffer(data), use_awaitable);
+
+      co_await async_write(state->client, buffer(data, n), use_awaitable);
+    }
+  }
+  catch (const std::exception& e)
+  {
+    state->client.close();
+    state->server.close();
+  }
+}
+
+awaitable<void> proxy(tcp::socket client, tcp::endpoint target)
+{
+  auto state = std::make_shared<proxy_state>(std::move(client));
+
+  co_await state->server.async_connect(target, use_awaitable);
+
+  auto ex = state->client.get_executor();
+  co_spawn(ex, client_to_server(state), detached);
+
+  co_await server_to_client(state);
+}
+
+awaitable<void> listen(tcp::acceptor& acceptor, tcp::endpoint target)
+{
+  for (;;)
+  {
+    auto client = co_await acceptor.async_accept(use_awaitable);
+
+    auto ex = client.get_executor();
+    co_spawn(ex, proxy(std::move(client), target), detached);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 5)
+    {
+      std::cerr << "Usage: proxy";
+      std::cerr << " <listen_address> <listen_port>";
+      std::cerr << " <target_address> <target_port>\n";
+      return 1;
+    }
+
+    boost::asio::io_context ctx;
+
+    auto listen_endpoint =
+      *tcp::resolver(ctx).resolve(
+          argv[1],
+          argv[2],
+          tcp::resolver::passive
+        );
+
+    auto target_endpoint =
+      *tcp::resolver(ctx).resolve(
+          argv[3],
+          argv[4]
+        );
+
+    tcp::acceptor acceptor(ctx, listen_endpoint);
+
+    co_spawn(ctx, listen(acceptor, target_endpoint), detached);
+
+    ctx.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+}
