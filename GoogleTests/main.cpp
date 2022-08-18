6d324a4b90e65f7ff22533dc28166214dd9fe0f9
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-08-18 18:57:31 +0000

    Partially added boost.

--- GoogleTests/main.cpp
+++ GoogleTests/main.cpp
@@ -1,6 +1,18 @@
 #include "gtest/gtest.h"
+#include <vector>
+#include <boost/uuid/detail/md5.hpp>
+
+using boost::uuids::detail::md5;
 
 int main(int argc, char **argv) {
+
+    md5 hash;
+    md5::digest_type digest;
+
+    std::vector<char> data {'1', '2', '3'};
+    hash.process_bytes(data.data(), data.size());
+    hash.get_digest(digest);
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
