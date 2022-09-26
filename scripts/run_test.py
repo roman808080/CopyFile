821ce5135ba2eeb1c829b6e530c3ab434b26a2a3
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-09-26 18:34:12 +0000

    Added temp files and hash calculations.

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -1,5 +1,7 @@
 #!/bin/env python3
 import random
+import tempfile
+import hashlib
 
 CHUNK_LOW_BOUNDARY = 1000
 CHUNK_UPPER_BOUNDARY = 2000
@@ -8,6 +10,7 @@ FILE_ITERATIONS_LOW_BOUNDARY = 100
 FILE_ITERATIONS_UPPER_BOUNDARY = 300
 
 MAX_UNICODE_UTF8 = 1112064
+READ_CHUNK_SIZE = 4096
 
 
 def _generate_chunk():
@@ -22,7 +25,7 @@ def _generate_chunk():
     return bytes(''.join(array_symbols), 'utf-8')
 
 
-def _generate_file_with_file_object(file_object):
+def generate_file_with_file_object(file_object):
     for _ in range(random.randrange(FILE_ITERATIONS_LOW_BOUNDARY, FILE_ITERATIONS_UPPER_BOUNDARY)):
         generated_chunk = _generate_chunk()
         file_object.write(generated_chunk)
@@ -30,11 +33,25 @@ def _generate_file_with_file_object(file_object):
 
 def generate_file(file_name):
     with open(file_name, 'wb') as file:
-        _generate_file_with_file_object(file)
+        generate_file_with_file_object(file)
+
+
+def calculate_hash_for_file(file_object):
+    sha256_hash = hashlib.sha256()
+    file_object.seek(0)
+
+    # Read and update hash string value in blocks of 4K
+    for byte_block in iter(lambda: file_object.read(READ_CHUNK_SIZE), b''):
+        sha256_hash.update(byte_block)
+
+    return sha256_hash.hexdigest()
 
 
 def main():
-    generate_file('generated.txt')
+    with tempfile.NamedTemporaryFile() as temp_file:
+        generate_file_with_file_object(temp_file)
+        hash_sha256 = calculate_hash_for_file(temp_file)
+        print(hash_sha256)
 
 
 if __name__ == '__main__':
