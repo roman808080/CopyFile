2461fa6c7793f20799bb5f06bf32141bc25c9a49
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-09-28 15:13:44 +0000

    Cleared tests.

--- GoogleTests/test.cpp
+++ GoogleTests/test.cpp
@@ -1,17 +1,6 @@
 #include "pch.h"
-#include "cryptopp/cryptlib.h"
-#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
-#include "cryptopp/md5.h"
-
-#include <iostream>
 
 TEST(TestCaseName, TestName) {
-    using namespace CryptoPP;
-    Weak::MD5 hash;
-    std::cout << "Name: " << hash.AlgorithmName() << std::endl;
-    std::cout << "Digest size: " << hash.DigestSize() << std::endl;
-    std::cout << "Block size: " << hash.BlockSize() << std::endl;
-
     EXPECT_EQ(1, 1);
     EXPECT_TRUE(true);
 }
