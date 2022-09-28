1130b3bf9978d9639037046bedb9b58a19f46ece
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-09-28 15:19:51 +0000

    Fixed formatting.

--- CopyFile/CopyFile.cpp
+++ CopyFile/CopyFile.cpp
@@ -9,35 +9,37 @@
 using namespace boost::program_options;
 namespace po = boost::program_options;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help,h", "produce help message")
-        ("source,s", po::value<std::string>(), "set source file")
-        ("destination,d", po::value<std::string>(), "set destination file")
-        ;
+	po::options_description desc("Allowed options");
+	desc.add_options()
+					("help,h", "produce help message")
+					("source,s", po::value<std::string>(), "set source file")
+					("destination,d", po::value<std::string>(), "set destination file");
 
 	po::variables_map variablesMap;
 
-    // parse regular options
-    po::store(po::parse_command_line(argc, argv, desc), variablesMap);
-    po::notify(variablesMap);
+	// parse regular options
+	po::store(po::parse_command_line(argc, argv, desc), variablesMap);
+	po::notify(variablesMap);
 
-    if (variablesMap.count("help") || argc == 1) {
-        std::cout << desc << std::endl;
-        return 0;
-    }
+	if (variablesMap.count("help") || argc == 1)
+	{
+		std::cout << desc << std::endl;
+		return 0;
+	}
 
-	std::string inputFileName {};
-    if (variablesMap.count("source")) {
+	std::string inputFileName{};
+	if (variablesMap.count("source"))
+	{
 		inputFileName = variablesMap["source"].as<std::string>();
-    }
+	}
 
-	std::string outputFileName {};
-    if (variablesMap.count("destination")) {
+	std::string outputFileName{};
+	if (variablesMap.count("destination"))
+	{
 		outputFileName = variablesMap["destination"].as<std::string>();
-    }
+	}
 
 	App app(inputFileName, outputFileName, Constants::Megabyte);
 	app.run();
