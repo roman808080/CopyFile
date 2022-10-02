251c6a623474bba10ae043530933cea3fe94fe14
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-10-02 18:57:37 +0000

    Implementing the rule of 5.

--- CopyFileLib/App.cpp
+++ CopyFileLib/App.cpp
@@ -112,9 +112,41 @@ namespace
 			this->region = std::move(mapped_region(this->shm, read_write));
 		}
 
+		SharedMemory(const SharedMemory&) = delete;
+		SharedMemory& operator=(const SharedMemory&) = delete;
+
+		SharedMemory(SharedMemory&& other)
+		: sharedMemoryName(std::move(other.sharedMemoryName))
+		, shm(std::move(other.shm))
+		, region(std::move(other.region))
+		, data{other.data}
+		{
+			other.data = nullptr;
+		}
+
+		SharedMemory& operator=(SharedMemory&& other)
+		{
+			if (&other == this)
+			{
+				return *this;
+			}
+
+			this->sharedMemoryName = std::move(other.sharedMemoryName);
+			this->shm = std::move(other.shm);
+			this->region = std::move(other.region);
+
+			this->data = other.data;
+			other.data = nullptr;
+
+			return *this;
+		}
+
 		~SharedMemory()
 		{
-			shared_memory_object::remove(this->sharedMemoryName.c_str());
+			if (sharedMemoryName != "")
+			{
+				shared_memory_object::remove(this->sharedMemoryName.c_str());
+			}
 		}
 
 		shared_memory_buffer* get()
@@ -142,7 +174,7 @@ namespace
 		}
 
 	private:
-		const std::string sharedMemoryName;
+		std::string sharedMemoryName;
 		shared_memory_object shm;
 		mapped_region region;
 		shared_memory_buffer* data;
