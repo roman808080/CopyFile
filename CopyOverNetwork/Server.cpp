8d1fd227e64c3617b21a53efcf4d880f7bf0e5eb
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-11-14 19:37:15 +0000

    Adding dummy implementation for the server.

--- CopyOverNetwork/Server.cpp
+++ CopyOverNetwork/Server.cpp
@@ -1,5 +1,34 @@
 #include "Server.h"
 
+#include <boost/asio.hpp>
+#include <boost/asio/read.hpp>
+
+using boost::asio::awaitable;
+using boost::asio::buffer;
+using boost::asio::co_spawn;
+using boost::asio::detached;
+using boost::asio::use_awaitable;
+using boost::asio::ip::tcp;
+
+namespace
+{
+    awaitable<void> handle_client(tcp::socket client)
+    {
+        // TODO: add implementation
+    }
+
+    awaitable<void> listen(tcp::acceptor &acceptor)
+    {
+        for (;;)
+        {
+            auto client = co_await acceptor.async_accept(use_awaitable);
+
+            auto ex = client.get_executor();
+            co_spawn(ex, handle_client(std::move(client)), detached);
+        }
+    }
+}
+
 Server::Server(const std::string &host, const std::string &port)
     : host(host),
       port(port)
@@ -8,7 +37,18 @@ Server::Server(const std::string &host, const std::string &port)
 
 void Server::run()
 {
-    // 
+    boost::asio::io_context ctx;
+
+    auto listen_endpoint =
+        *tcp::resolver(ctx).resolve(
+            "0.0.0.0",
+            "5555",
+            tcp::resolver::passive);
+
+    tcp::acceptor acceptor(ctx, listen_endpoint);
+    co_spawn(ctx, listen(acceptor), detached);
+
+    ctx.run();
 }
 
 Server::~Server()
