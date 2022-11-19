32469b127a03fef26b30858be7fc3890c2da492a
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-11-19 18:58:52 +0000

    Typed some text.

--- CopyOverNetwork/Server.cpp
+++ CopyOverNetwork/Server.cpp
@@ -10,11 +10,57 @@ using boost::asio::detached;
 using boost::asio::use_awaitable;
 using boost::asio::ip::tcp;
 
+////////////////////////////////////////////////
+// On TCP Level
+////////////////////////////////////////////////
+// | size | char[] | -> the Client class has only shallow understanding about our protocol
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// Protocol
+////////////////////////////////////////////////
+// 0 | int |        -> ping (int == 1)
+// 1 | str |        -> client name
+// 2 | int | str |  -> file name. Int == file_name_size, str == file_name
+// 3 | int |        -> permissions
+// 4 | int |        -> options. 001 -> allows rewriting for a given client.
+// 5 | int | str |  -> file block. First parameter the size of the block, the second char[]
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// Handler
+// private:
+// buffer
+////////////////////////////////////////////////
+
 namespace
 {
+    class Protocol
+    {
+    public:
+        Protocol()
+        {}
+
+        void onReceivePackage(std::size_t packageSize, const std::array<char, 1024>& package)
+        {}
+
+    private:
+        // 
+
+    };
+    
     awaitable<void> handle_client(tcp::socket client)
     {
-        // TODO: add implementation
+        std::array<char, 1024> data{0};
+        std::size_t next_block_size = 0;
+
+        co_await boost::asio::async_read(client, buffer(data, sizeof(next_block_size)), use_awaitable);
+        memcpy(&next_block_size, &data, sizeof(next_block_size));
+
+        if (next_block_size > data.size())
+        {
+            throw std::runtime_error("Next block size is more then 1024 bytes.");
+        }
     }
 
     awaitable<void> listen(tcp::acceptor &acceptor)
